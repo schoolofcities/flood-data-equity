8c557209a63ca4a57ca0840dab6552385c29ef71
dlm589 <dlm589@yahoo.com.tw>
2023-11-16 14:12:53 -0500

    File Structure update

---
+++ flood-data-mapping/.svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/map-art-type/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/map-art-type/+page.svelte";
